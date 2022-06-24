7af48762fdd451843c52e3a07a4cc976ad3d13b4
qqq147 <2083093793@qq.com>
2022-06-24 08:17:17 +0800

    suibi

---
+++ demo/diaries.js
@@ -0,0 +1,160 @@
+var diaries = [
+  {
+    meta: {  // 内容元数据
+      cover: "https://obohe.com/i/2022/06/23/11auv7k.jpg",
+      avatar: "https://obohe.com/i/2022/06/23/11auv7k.jpg",
+      title: "期末周实录",
+      meta: "2022.06.22",
+      create_time: "2022.06.22 23：24",
+      nickName: "白马过平川",
+    },
+    list: [
+      {
+        type: "TEXT",
+        content: '期末周......真的会谢',
+        poi: {
+          longitude: 117.2,
+          latitude: 37.5,
+          name: "荆州市",
+        },
+        description: "",
+        id: 1,
+        commentNum: 0,
+        likeNum: 0,
+      },
+      {
+        type: "IMAGE",
+        content: 'https://obohe.com/i/2022/06/23/116pqen.jpg',
+        poi: {
+          longitude: 117.2,
+          latitude: 37.5,
+          name: "荆州市",
+        },
+        description: "模电好难",
+        id: 2,
+        commentNum: 1,
+        likeNum: 5,
+      },
+      {
+          type: "IMAGE",
+          content: 'https://obohe.com/i/2022/06/23/117ole5.jpg',
+          poi: {
+              longitude: 117.2,
+              latitude: 37.5,
+              name: "荆州市",
+          },
+          description: "6月23号考数据结构",
+          id: 2,
+          commentNum: 1,
+          likeNum: 5,
+      },
+      {
+        type: "IMAGE",
+        content: 'https://obohe.com/i/2022/06/23/117xus4.jpg',
+        poi: {
+          longitude: 117.2,
+          latitude: 37.5,
+          name: "荆州市",
+        },
+        description: "6月25号考模电",
+        id: 2,
+        commentNum: 1,
+        likeNum: 5,
+      },
+      {
+        type: "IMAGE",
+        content: 'https://obohe.com/i/2022/06/23/119od62.jpg',
+        poi: {
+          longitude: 117.2,
+          latitude: 37.5,
+          name: "荆州市",
+        },
+        description: "",
+        id: 2,
+        commentNum: 1,
+        likeNum: 5,
+      },
+    ]
+  },
+  {
+    meta: {  // 内容元数据
+      cover: "https://s1.ax1x.com/2022/05/06/OnBwvj.jpg",
+      avatar: "https://s1.ax1x.com/2022/05/06/Onwol9.jpg",
+      title: "一些个表情包",
+      meta: "2022.06.24",
+      create_time: "2022.06.24 11:57:27",
+      nickName: "十夏",
+    },
+    list: [
+      {
+        type: "IMAGE",
+        content: 'https://s1.ax1x.com/2022/05/06/OnBwvj.jpg',
+        poi: {
+          longitude: 117.2,
+          latitude: 37.5,
+          name: "荆州市",
+        },
+        description: "",
+        id: 2,
+        commentNum: 1,
+        likeNum: 5,
+      },
+      {
+        type: "IMAGE",
+        content: 'https://s1.ax1x.com/2022/05/06/OnDUdx.jpg',
+        poi: {
+          longitude: 117.2,
+          latitude: 37.5,
+          name: "荆州市",
+        },
+        description: "",
+        id: 2,
+        commentNum: 1,
+        likeNum: 5,
+      },
+      {
+        type: "IMAGE",
+        content: 'https://s1.ax1x.com/2022/05/06/OnrIN6.jpg',
+        poi: {
+          longitude: 117.2,
+          latitude: 37.5,
+          name: "荆州市",
+        },
+        description: "",
+        id: 2,
+        commentNum: 1,
+        likeNum: 5,
+      },
+      {
+        type: "IMAGE",
+        content: 'https://s1.ax1x.com/2022/05/06/OnrOud.jpg',
+        poi: {
+          longitude: 117.2,
+          latitude: 37.5,
+          name: "荆州市",
+        },
+        description: "",
+        id: 2,
+        commentNum: 1,
+        likeNum: 5,
+      },
+      {
+        type: "IMAGE",
+        content: 'https://s1.ax1x.com/2022/05/06/OnsADs.jpg',
+        poi: {
+          longitude: 117.2,
+          latitude: 37.5,
+          name: "荆州市",
+        },
+        description: "",
+        id: 2,
+        commentNum: 1,
+        likeNum: 5,
+      },
+    ]
+  }
+]
+
+module.exports = {
+  diaries: diaries,
+}
